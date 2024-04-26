bea14ad3cbc7222242b498400273de9408f8e04d
asphodel42 <asphodel42@gmail.com>
2024-04-26 19:10:41 +0300

    refactoring code

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -4,36 +4,36 @@ from DQN import Agent
 import numpy as np
 import pandas as pd
 
-
 # Assets
 image_background    = 'SpaceInvaders/assets/background/galaxy.jpg'  # Background
 image_ship          = 'SpaceInvaders/assets/sprites/space_ship_state.png'  # Player
 image_bullet        = 'SpaceInvaders/assets/sprites/laser.png'  # Bullets
 image_alien         = 'SpaceInvaders/assets/sprites/alien.png'  # Enemy
-sound_music         = 'SpaceInvaders/assets/music/space.ogg'  # Music
-sound_shoot         = 'SpaceInvaders/assets/music/shoot_sound.ogg'
-sound_explosion     = 'SpaceInvaders/assets/music/explosion.ogg'  # Explosion
 image_icon          = 'SpaceInvaders/assets/background/icon.png'  # Icon
 font_name           = "SpaceInvaders/assets/font/Starjout.ttf"  # Font
+
 
 class GameSprite(pygame.sprite.Sprite):
     """Main class for sprites"""
     def __init__(self, window, win_height, win_width, 
-                 player_image, player_x, player_y, width, height, speed):  # Initialization
+                 player_image, cord_x, cord_y, width, height, speed):
         pygame.sprite.Sprite.__init__(self)
+
+        # Window
         self.window = window
         self.window_height = win_height
         self.window_width = win_width
 
+        # Sprite object
         self.image = pygame.transform.scale(pygame.image.load(
-            player_image), (width, height))  # Sprite object
+            player_image), (width, height))  
 
-        self.speed = speed  # Speed
+        self.speed = speed
 
         # Object's hitbox
         self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
+        self.rect.x = cord_x
+        self.rect.y = cord_y
 
     def getPos(self):
         return [self.rect.centerx, self.rect.centery]
@@ -70,6 +70,7 @@ class Player(GameSprite):
                          image_bullet, self.rect.centerx, self.rect.y, 10, 60, 100)
         return bullet
     
+
 class Alien(GameSprite):
     def __init__(self, ship, bullets, window, win_height, win_width, 
                  player_image, player_x, player_y, width, height, speed):
@@ -79,18 +80,18 @@ class Alien(GameSprite):
         self.bullets = bullets
         self.pos_x = 0
         self.pos_y = 0
-        
-    def updateMove(self):
-        self.rect.y += self.speed
-        # Disapear if crossed the edge of window
-        if self.rect.y > self.window_height:
-            return True
-        else: return False
-    
+
     def collissionShip(self, ship):
         if self.rect.colliderect(ship.rect):
             return True
+        
+    def update(self):
+        self.rect.y += self.speed
 
+    def updateMove(self):
+        if self.rect.y > self.window_height or self.collissionShip(self.ship):
+            return True
+    
     def collisionBullet(self):
         for bullet in self.bullets:
             if self.rect.colliderect(bullet.rect):
@@ -106,14 +107,18 @@ class Bullet(GameSprite):
             self.kill()
         
 class Game():
-    def __init__(self):
+    def __init__(self, num_games, gamma, lr):
         # Vars
         self.score_points = 0
+        self.record_score_points = 0
         self.reward = 0
+        self.n_games = num_games
+        self.gamma = gamma
+        self.lr = lr
 
         self.finish = False
         self.game = True
-        self.record_score_points = 0
+        
         self.previous_time = pygame.time.get_ticks()
         
         # Create a window
@@ -122,14 +127,14 @@ class Game():
         pygame.display.set_caption("Space War")
         pygame.display.set_icon(pygame.image.load(image_icon))
         self.window = pygame.display.set_mode((self.window_width, self.window_height))
-
-        # Creating background
         self.background = pygame.transform.scale(pygame.image.load(image_background),
                                             (self.window_width, self.window_height))
         
         # Label
         pygame.font.init()
         self.font = pygame.font.Font(font_name, 25)
+        self.record_score = self.font.render(f'Record: {self.record_score_points}', True, (255, 232, 31))
+        self.score = self.font.render(f'Score: {self.score_points}', True, (255, 232, 31))
 
         # Creating star ship
         self.ship = Player(self.window, self.window_height, self.window_width,
@@ -141,13 +146,16 @@ class Game():
         # Creating aliens
         self.createAliens()
 
-        # TODO: get observation of a display
+        self.createAgent(self.gamma, self.lr)
+
         self.observation = self.getObjectsPos()
         self.observation_ = self.getObjectsPos()
 
-        self.dataFrame = pd.DataFrame(columns=['Episode','Score', 'Epsilon', 'Gamma', 'Alpha'])
-        
+        self.dataFrame = pd.DataFrame(columns=['Episode','Score', 'Record', 'Epsilon', 'Gamma', 'Alpha'])
 
+    def createAgent(self, gamma, lr):
+        self.agent = Agent(gamma, lr, epsilon=1,  eps_end=0.05, eps_dec=5e-4, 
+                              batch_size=64, n_actions=3, input_dims=[18,])
 
     def getObjectsPos(self):
         cordArray = np.zeros(2 * 9, dtype=np.float32)
@@ -164,16 +172,21 @@ class Game():
     def createAliens(self):
         for _ in range(1, 6):
             alien = Alien(self.ship, self.bullets, self.window, self.window_height, self.window_width,
-                            image_alien, randint(60, self.window_width-60), -40, 50, 50, randint(4, 8))
+                            image_alien, randint(60, self.window_width-60), -40, 50, 50, 4)
             self.aliens.add(alien)
 
-    def alienCollision(self):
-        for alien in self.aliens:
-            if alien.collissionShip(self.ship) or alien.updateMove():
-                self.aliens.empty()
-                return True
+    def drawSprites(self):
+        self.bullets.draw(self.window)
+        self.aliens.draw(self.window)
+        self.ship.reset()
+
+    def blitWindow(self):
+        # Update background
+        self.window.blit(self.background, (0,0))  # Background
+        self.window.blit(self.record_score, (10, 0))  # Record score label
+        self.window.blit(self.score, (10, 25))  # Score label
             
-    def addGameDataToDataFrame(df, episode, score, record, epsilon, gamma, alpha):
+    def addToDataFrame(self, df, episode, score, record, epsilon, gamma, alpha):
         new_row = pd.DataFrame({
             'Episode': [episode],
             'Score': [score],
@@ -184,13 +197,8 @@ class Game():
         })
         df = pd.concat([df, new_row], ignore_index=True)
         return df
-        
-
+    
     def game_loop(self):
-        agent = Agent(gamma=0.99, epsilon=1, eps_end=0.05, eps_dec=5e-4, lr=0.001, 
-                              batch_size=64, n_actions=3, input_dims=[18,])
-        self.n_games = 500
-        self.scores, self.eps_history = [], []
         for episode in range(1, self.n_games):
             self.game = True
             self.finish = False
@@ -198,53 +206,38 @@ class Game():
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         quit()
-                if not self.finish:
-                    # Render fonts
-                    self.record_score = self.font.render(f'Record: {self.record_score_points}', True, (255, 232, 31))
-                    self.score = self.font.render(f'Score: {self.score_points}', True, (255, 232, 31))
-                    
-                    # Update background
-                    self.window.blit(self.background, (0,0))  # Background
-                    self.window.blit(self.record_score, (10, 0))  # Record score label
-                    self.window.blit(self.score, (10, 25))  # Score label
-                    
-                    # Making action
+                if not self.finish:        
+                    self.blitWindow()
                     self.reward = 0
-                    self.action = agent.choose_action(self.observation)
-
+                    self.action = self.agent.choose_action(self.observation)
                     self.ship.update(self.action, self.bullets)
                     self.bullets.update()
                     self.aliens.update()
-
                     # Sprites draw
-                    self.bullets.draw(self.window)
-                    self.aliens.draw(self.window)
-                    self.ship.reset()
-
-                    # Update movement
+                    self.drawSprites()
                     for alien in self.aliens:
                         if alien.collisionBullet():
                             self.score_points += 1
                             self.reward += 100
-
-                    # Losing
-                    if self.alienCollision():
-                        self.finish = True
-                        self.reward -= 1000
-                        if self.score_points > self.record_score_points:
-                            self.record_score_points = self.score_points
-                            
-
+                            self.score = self.font.render(f'Score: {self.score_points}',
+                                                           True, (255, 232, 31))
+                        elif alien.updateMove():
+                            self.aliens.empty()
+                            self.finish = True
+                            self.reward -= 1000
+                            if self.score_points > self.record_score_points:
+                                self.record_score_points = self.score_points
+                                self.record_score = self.font.render(f'Record: {self.record_score_points}',
+                                                                    True, (255, 232, 31))
                     self.observation_ = self.getObjectsPos()
-                    agent.store_transition(self.observation, self.action, self.reward,
+                    self.agent.store_transition(self.observation, self.action, self.reward,
                                         self.observation_, self.finish)
-                    agent.learn()
+                    self.agent.learn()
                     self.observation = self.observation_
                 else:
-                    # print("Episode", episode, " | Score ", self.score_points, " | Epsilon %.2f " % agent.epsilon, " | Record ", self.record_score_points)
-                    self.dataFrame = self.addGameDataToDataFrame(self.dataFrame, episode, self.score_points, 
-                                                            self.record_score_points, agent.epsilon, 
-                                                            agent.gamma, agent.lr)
+                    self.dataFrame = self.addToDataFrame(self.dataFrame, episode, self.score_points, 
+                                                            self.record_score_points, self.agent.epsilon, 
+                                                            self.agent.gamma, self.agent.lr)
                     print(self.dataFrame)
                     self.score_points = 0
                     self.createAliens()
@@ -253,5 +246,5 @@ class Game():
                 pygame.time.delay(40)
 
              
-GamePlay = Game()
+GamePlay = Game(500, 0.99, 0.001)
 GamePlay.game_loop()
