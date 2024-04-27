7de5d168d3c3c0e57991fc663732fdf7058be1e7
asphodel42 <asphodel42@gmail.com>
2024-04-27 21:24:42 +0300

    update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import pymysql
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def create_connection(host, user, password, database):
     try:
@@ -29,11 +30,11 @@ def create_database(host, user, password, database):
     except Exception as e:
         print("Error:", e)
 
-def create_table(connection):
+def create_table(connection, table):
     try:
         with connection.cursor() as cursor:
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS game_data (
+            cursor.execute(f"""
+                CREATE TABLE IF NOT EXISTS {table} (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     episode INT,
                     score INT,
@@ -52,13 +53,11 @@ def create_table(connection):
         print("Error:", e)
 
 
-def insert_data(connection, episode, score, record, epsilon, gamma, alpha, duration):
-    # gammaAlpha = gamma / alpha
-    # alphaGamma = alpha / gamma
+def insert_data(connection, table, episode, score, record, epsilon, gamma, alpha, duration):
     try:
         with connection.cursor() as cursor:
-            cursor.execute("""
-                INSERT INTO game_data (episode, score, record, epsilon, 
+            cursor.execute(f"""
+                INSERT INTO {table} (episode, score, record, epsilon, 
                            gamma, alpha, gammaAlpha, alphaGamma, duration)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
             """, (episode, score, record, epsilon, gamma, alpha,
@@ -83,8 +82,7 @@ def addToDataFrame(df, episode, score, record, epsilon, gamma, alpha, duration):
         df = pd.concat([df, new_row], ignore_index=True)
         return df
 
-def createPlot(filename, episode, score, mean):
-    # Створення графіка
+def createPlot(episode, score, mean, filename):
     fig, ax1 = plt.subplots()
 
     # Побудова кривої для кількості набраних очок
@@ -100,5 +98,39 @@ def createPlot(filename, episode, score, mean):
     ax2.set_ylabel('Mean', color=color)
     ax2.plot(episode, mean, color=color)
     ax2.tick_params(axis='y', labelcolor=color)
-
     plt.savefig(filename)
+    plt.close()
+
+# def createPlot(x, scores, mean, filename, lines=None):
+#     fig=plt.figure()
+#     ax=fig.add_subplot(111, label="1")
+#     ax2=fig.add_subplot(111, label="2", frame_on=False)
+
+#     ax.plot(x, scores, color="C0")
+#     ax.set_xlabel("Game", color="C0")
+#     ax.set_ylabel("Score", color="C0")
+#     ax.tick_params(axis='x', colors="C0")
+#     ax.tick_params(axis='y', colors="C0")
+
+#     # N = len(scores)
+#     # running_avg = np.empty(N)
+#     # for t in range(N):
+#     #     running_avg[t] = np.mean(scores[max(0, t-20):(t+1)])
+
+#     ax2.plot(x, mean, color="C1")
+#     #ax2.xaxis.tick_top()
+#     ax2.axes.get_xaxis().set_visible(False)
+#     ax2.yaxis.tick_right()
+#     #ax2.set_xlabel('x label 2', color="C1")
+#     ax2.set_ylabel('Mean', color="C1")
+#     #ax2.xaxis.set_label_position('top')
+#     ax2.yaxis.set_label_position('right')
+#     #ax2.tick_params(axis='x', colors="C1")
+#     ax2.tick_params(axis='y', colors="C1")
+
+#     if lines is not None:
+#         for line in lines:
+#             plt.axvline(x=line)
+
+#     plt.savefig(filename)
+#     plt.close()
