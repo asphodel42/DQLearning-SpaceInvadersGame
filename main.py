e702574b9e425cb65bd8fa5138e02509b97f36fe
asphodel42 <asphodel42@gmail.com>
2024-04-27 21:25:22 +0300

    update main file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from random import randint
 import time
 import numpy as np
 import pandas as pd
-import asyncio
 from DQN import Agent
 from database import create_connection, create_database, create_table, insert_data, addToDataFrame, createPlot
 from SpaceInvaders import Player, Alien
@@ -16,16 +15,17 @@ image_alien         = 'assets/sprites/alien.png'  # Enemy
 image_bullet        = 'assets/sprites/laser.png'  # Bullets
   
 class Game():
-    def __init__(self, num_games, gamma, lr):
+    def __init__(self, num_games, gamma, lr, tablename):
         # Connection to database
         host = 'localhost'
         user = 'root'
         password = '0000'
         database = 'dql_data'
+        self.tablename = tablename
 
         create_database(host, user, password, database)
         self.connection = create_connection(host, user, password, database)
-        create_table(self.connection)
+        create_table(self.connection, self.tablename)
 
         # Vars
         self.score_points = 0
@@ -57,7 +57,7 @@ class Game():
 
         # Creating star ship
         self.ship = Player(self.window, self.window_height, self.window_width,
-                            image_ship, image_bullet, self.window_width / 2, self.window_height-50, 60, 50, 8)
+                            image_ship, image_bullet, self.window_width / 2, self.window_height-50, 60, 50, 5)
         # Creating groups for objects
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -75,24 +75,24 @@ class Game():
 
     def createAgent(self, gamma, lr):
         self.agent = Agent(gamma, lr, epsilon=1,  eps_end=0.05, eps_dec=5e-4, 
-                              batch_size=64, n_actions=3, input_dims=[18,])
-
+                              batch_size=64, n_actions=3, input_dims=[36,])
+  
     def getObjectsPos(self):
-        cordArray = np.zeros(2 * 9, dtype=np.float32)
-        cordArray[0], cordArray[1] = self.ship.getPos()
-        index = 2
+        cordArray = np.zeros(2 * 18, dtype=np.float32)
+        cordArray[0], cordArray[1], cordArray[2], cordArray[3] = self.ship.getPos()
+        index = 4
         for alien in self.aliens:
-            cordArray[index], cordArray[index+1] = alien.getPos()
-            index += 2
+            cordArray[index], cordArray[index+1], cordArray[index+2], cordArray[index+3] = alien.getPos()
+            index += 4
         for bullet in self.bullets:
-            cordArray[index], cordArray[index+1] = bullet.getPos()
-            index += 2
-        return cordArray    
+            cordArray[index], cordArray[index+1], cordArray[index+2], cordArray[index+3] = bullet.getPos()
+            index += 4
+        return cordArray 
     
     def createAliens(self):
         for _ in range(1, 6):
             alien = Alien(self.ship, self.bullets, self.window, self.window_height, self.window_width,
-                            image_alien, randint(60, self.window_width-60), -40, 50, 50, 4)
+                            image_alien, randint(60, self.window_width-60), -40, 50, 50, randint(3, 6))
             self.aliens.add(alien)
 
     def drawSprites(self):
@@ -108,26 +108,32 @@ class Game():
     
     def game_loop(self):
         startLearnTime = time.time()
-        
         for episode in range(1, self.n_games+1):
             startEpisodeTime = time.time()
             self.game = True
             self.finish = False
             while self.game:  # Game loop
+                self.score = self.font.render(f'Score: {self.score_points}',
+                                                           True, (255, 232, 31))
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         quit()
+                self.reward = 0
                 if not self.finish:        
-                    self.blitWindow()
-                    self.reward = 0
+                    self.blitWindow() 
                     self.action = self.agent.choose_action(self.observation)
                     self.ship.update(self.action, self.bullets)
                     self.bullets.update()
                     self.aliens.update()
                     # Sprites draw
                     self.drawSprites()
+                    # if self.action == 0 or self.action == 1:
+                    #     self.reward -= 1
                     for alien in self.aliens:
-                        if alien.collisionBullet():
+                        if alien.rect.y >= self.window_height:
+                            alien.rect.y = - 20
+                            alien.rect.x = randint(50, self.window_width-50)
+                        elif alien.collisionBullet():
                             self.score_points += 1
                             self.reward += 100
                             self.score = self.font.render(f'Score: {self.score_points}',
@@ -135,7 +141,7 @@ class Game():
                         elif alien.updateMove():
                             self.aliens.empty()
                             self.finish = True
-                            self.reward -= 1000
+                            self.reward -= 200
                             if self.score_points > self.record_score_points:
                                 self.record_score_points = self.score_points
                                 self.record_score = self.font.render(f'Record: {self.record_score_points}',
@@ -151,18 +157,18 @@ class Game():
                     self.dataFrame = addToDataFrame(self.dataFrame, episode, self.score_points, 
                                                             self.record_score_points, self.agent.epsilon, 
                                                             self.agent.gamma, self.agent.lr, episodeDuration)
-                    insert_data(self.connection, episode, self.score_points, self.record_score_points,
+                    insert_data(self.connection, self.tablename, episode, self.score_points, self.record_score_points,
                                      self.agent.epsilon, self.agent.gamma, self.agent.lr, episodeDuration)
-                    createPlot('QLearning.png', self.dataFrame["Episode"], self.dataFrame["Score"],
-                                self.dataFrame["Mean"])
+                    createPlot(self.dataFrame["Episode"].to_list(), self.dataFrame["Score"].to_list(),
+                                self.dataFrame["Mean"].to_list(), 'DQLearning.png',)
                     self.score_points = 0
                     self.createAliens()
                     self.game=False 
                 pygame.display.update()
-                pygame.time.delay(40)
+                pygame.time.delay(5)
         endLearnTime = time.time()
         learnDuration = endLearnTime - startLearnTime
 
 if __name__ == "__main__":      
-    GamePlay = Game(5000, 0.7, 0.2)
+    GamePlay = Game(5000, 0.99, 0.001, 'game_dat_test2')
     GamePlay.game_loop()
