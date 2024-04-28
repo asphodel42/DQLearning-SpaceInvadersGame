c90cd947aa843b7dd0ebb3dd6e4ab4760c401208
asphodel42 <asphodel42@gmail.com>
2024-04-28 22:25:35 +0300

    test version of creating matrix from db

---
+++ getData.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from database import create_connection, fetch_data_to_dataframe, createScatterMatrix, createCorrelationMatrix
+
+# Connection parameters
+host = 'localhost'
+user = 'root'
+password = '0000'
+database = 'dql_data'
+table = 'game_dat_test2'
+
+connection = create_connection(host, user, password, database)
+
+df = fetch_data_to_dataframe(connection, table)
+df_statisitc = df.loc[:, ['episode', 'score', 'record', 'epsilon', 'duration']]
+
+createScatterMatrix(df_statisitc, 'statistics\scatter-matrix\scatterTest.jpg')
+createCorrelationMatrix(df_statisitc, 'statistics\correlation-Matrix\correlationTest.jpg')
