28384c0794694b170257cbcdb976df9aae292b7a
asphodel42 <asphodel42@gmail.com>
2024-05-01 23:12:29 +0300

    stored data

--- getData.py
+++ getData.py
@@ -5,12 +5,12 @@ host = 'localhost'
 user = 'root'
 password = '0000'
 database = 'dql_data'
-table = 'game_dat_test2'
+table = 'game_dat_test_099_0001'
 
 connection = create_connection(host, user, password, database)
 
 df = fetch_data_to_dataframe(connection, table)
-df_statisitc = df.loc[:, ['episode', 'score', 'record', 'epsilon', 'duration']]
+df_statisitc = df.loc[:, ['score','episode']]
 
-createScatterMatrix(df_statisitc, 'statistics\scatter-matrix\scatterTest.jpg')
-createCorrelationMatrix(df_statisitc, 'statistics\correlation-Matrix\correlationTest.jpg')
+createScatterMatrix(df_statisitc, 'statistics\scatter-matrix\scatterTest1.jpg', True)
+# createCorrelationMatrix(df_statisitc, 'statistics\correlation-Matrix\correlationTest.jpg')
